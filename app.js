ab7a2203419f32bd7ddbe1ee0dd75f9b033fbce8
gao.xiaojuan <gao.xiaojuan@cmge.com>
2019-03-26 19:16:05 +0800

    授权登录功能

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 //app.js
 App({
+  data:{
+    code:''
+  },
   onLaunch: function () {
     // 展示本地存储能力
     var logs = wx.getStorageSync('logs') || []
@@ -9,6 +12,8 @@ App({
     // 登录
     wx.login({
       success: res => {
+          this.code = res.code;
+        console.log(res);
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
       }
     })
@@ -16,12 +21,24 @@ App({
     wx.getSetting({
       success: res => {
         if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
           wx.getUserInfo({
             success: res => {
-              // 可以将 res 发送给后台解码出 unionId
+              console.log(res);
               this.globalData.userInfo = res.userInfo
-
+              const { encryptedData, iv } = res;
+              wx.request({
+                url: this.globalData.url +'users/wxLogin',
+                method:'post',
+                data: {
+                  "code": this.code,
+                  "encryptedData": encryptedData,
+                  "iv": iv
+                },
+                success(res) {
+                  let jwt = res.data;
+                  wx.setStorageSync('jwt', jwt)
+                }
+              })
               // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
               // 所以此处加入 callback 以防止这种情况
               if (this.userInfoReadyCallback) {
@@ -34,6 +51,7 @@ App({
     })
   },
   globalData: {
+    url:'https://www.ableya.cn/',
     userInfo: null
   }
 })
